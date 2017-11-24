084c56dfab6b75916f3ad15b5fdb576e05877948
Sravanthi Kanchi <shravik593@gmail.com>
2017-11-23 23:55:21 -0500

    config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+
+
+module.exports = {
+    consumer_key:         'cUvtJdr5fyw3LkSc6mvKb0B9o',
+  consumer_secret:      '4OMTS2P78AxRPrqEBjNproyAjgrbWAxfkyQdeW58A4tynDipTJ',
+  access_token:         '895595146422525952-2UZ5uSUV8OENAEUtLRMEQd52RfTw5jY',
+  access_token_secret:  'qs6UNvh7F8y5tX13X94itpDB4S40Hm3et2KkxH4iThEGz',
+}
